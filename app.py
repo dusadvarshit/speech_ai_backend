816623a5bfe540d7e1a8ba6beb2e72207ceb13ca
dusadvarshit <dusadv@gmail.com>
2024-09-21 07:21:08 +0000

    Modified app file to include database models

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, Response
+from flask_cors import CORS
 import os
 import boto3
 from aws_helper import upload_file_to_audios
 from pause_finder import pause_main
 from general_helpers import convert_webm_to_wav
+import json
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
+
 
 ## Accessing .env file
 from dotenv import load_dotenv
@@ -12,6 +18,17 @@ load_dotenv()
 ####
 
 app = Flask(__name__)
+# Allow requests from the React app
+CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
+
+# Configure PostgreSQL database
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
+
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+jwt = JWTManager(app)
 
 # Ensure the upload directory exists
 UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.realpath(__file__)),
@@ -22,8 +39,13 @@ os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 def index():
     return render_template('index.html')
 
+@app.route('/hello')
+def hello():
+    return Response("{'hello':'hello'}", status=201, mimetype='application/json')
+
 @app.route('/upload', methods=['POST'])
 def upload():
+    print(request)
     if 'file' not in request.files:
         return 'No file part'
     
@@ -46,13 +68,19 @@ def upload():
             convert_webm_to_wav(input_file, output_file)
 
         ## Upload file to AWS S3
-        upload_file_to_audios(file)
+        # upload_file_to_audios(file)
 
         ## Find Pauses
         pause_info, t = pause_main(file_path)
         print(pause_info)
 
         return 'File uploaded successfully!'
+
+@app.route('/list', methods=['get'])
+def list():
+    file_list = os.listdir("./uploads")
+
+    return {'data': file_list}
 
 if __name__ == '__main__':
     app.run(debug=True)
